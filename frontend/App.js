4cff1700ce64d96c28aaa055ae4a862d97e2b221
Luke Inglis <lukeminglis@tamu.edu>
2022-10-13 22:45:43 -0500

    Files for my login concept
    This will replace the example program "App.js" in the frontend folder with my login concept of the same name. All assets are included, with additional prototype background images & logos that should be removed when a final decision has been made.

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,151 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    Image,
+    TextInput,
+    Dimensions,
+    TouchableOpacity,
+   } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+import bgImage from './assets/images/reveillebg.png'
+import logo from './assets/images/pawLogo.png'
+
+
+const { width: WIDTH } = Dimensions.get('window')
+
+export default class Example extends Component {
+  render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={bgImage} style={styles.backgroundContainer} blurRadius={4}>
+      <View style={styles.logoContainer}>
+        <Image 
+          source={logo} 
+          style={styles.logo}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Icon
+          style={styles.inputIcon}
+          name={'person-outline'} 
+          size={28} 
+          color ={'rgba(0, 0, 0, 0.7)'}
+        />
+        <TextInput 
+            style={styles.input}
+            placeholder={'Username'}
+            placeholderTextColor={'rgba(119, 119, 119, 0.7)'}
+            underlineColorAndroid='transparent'
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Icon
+          style={styles.inputIcon}
+          name={'lock-closed-outline'} 
+          size={28} 
+          color ={'rgba(0, 0, 0, 0.7)'}
+        />
+        <TextInput 
+            style={styles.input}
+            placeholder={'Password'}
+            secureTextEntry={true}
+            placeholderTextColor={'rgba(119, 119, 119, 0.7)'}
+            underlineColorAndroid='transparent'
+        />
+
+        <TouchableOpacity style={styles.btnEye}>
+          <Icon
+           name={'ios-eye-off-outline'} 
+           size={26} 
+           color={'rgba(255, 255, 255, 0.7'}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
+
+      <View style={{marginTop: 20, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+        <View>
+          <Text style={{width: 50, textAlign: 'center'}}>OR</Text>
+        </View>
+        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+      </View>
+
+      <TouchableOpacity style={styles.btnCreateAcc}>
+          <Text style={styles.text}>Create an Account</Text>
+      </TouchableOpacity>
+
+    </ImageBackground>
   );
+}
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: null,
+    height: null,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50
+  },
+  logo: {
+    width: 250,
+    height: 250
+  },
+  inputContainer: {
+    marginTop: 10,
+  },
+  input: {
+      width: WIDTH - 55,
+      height: 45,
+      borderRadius: 25,
+      fontSize: 18,
+      paddingLeft: 45,
+      backgroundColor: 'rgba(255, 255, 255, 0.4)',
+      color: 'rgba(119, 119, 119, 0.7)',
+      marginHorizontal: 25,
+  },
+  inputIcon: {
+    position: 'absolute',
+    top: 8,
+    left: 37
+  },
+  btnEye: {
+    position: 'absolute',
+    top: 8,
+    right: 37
+  },
+  btnLogin: {
+    width: WIDTH - 200,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    justifyContent: 'center',
+    marginTop: 20
+  },
+  text: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    fontsize: 20,
+    textAlign: 'center',
+  },
+  btnCreateAcc: {
+    width: WIDTH - 200,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    justifyContent: 'center',
+    marginTop: 20
   },
 });
